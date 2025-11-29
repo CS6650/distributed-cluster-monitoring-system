b1500cd2c8d71d9f2bae84a39c9e6233cac467f2
darshpandya02 <pandya.dar@northeastern.edu>
2025-11-29 18:56:10 -0500

    implemented raft replication, and leader election

--- common/timer.hpp
+++ common/timer.hpp
@@ -5,23 +5,53 @@
 #include <functional>
 #include <thread>
 #include <atomic>
+#include <memory>
 
-class Timer {
-    std::atomic<bool> running {false};
+class Timer
+{
+    // Use shared_ptr so all threads see the same atomic
+    std::shared_ptr<std::atomic<bool>> running;
     std::thread th;
 
 public:
-    template<typename Fn>
-    void start(int ms, Fn callback) {
-        running = true;
-        th = std::thread([=]() {
+    Timer() : running(std::make_shared<std::atomic<bool>>(false)) {}
+
+    ~Timer()
+    {
+        cancel();
+        // Don't join detached threads
+    }
+
+    template <typename Fn>
+    void start(int ms, Fn callback)
+    {
+        // Cancel any existing timer
+        cancel();
+
+        // Create new running flag
+        running = std::make_shared<std::atomic<bool>>(true);
+
+        // Capture running by value (but it's a shared_ptr, so all copies point to same atomic)
+        auto runningCopy = running;
+
+        th = std::thread([runningCopy, ms, callback]()
+                         {
             std::this_thread::sleep_for(std::chrono::milliseconds(ms));
-            if (running) callback();
-        });
+            
+            // Check if timer was cancelled
+            if (runningCopy->load()) {
+                callback();
+            } });
         th.detach();
     }
 
-    void cancel() { running = false; }
+    void cancel()
+    {
+        if (running)
+        {
+            running->store(false);
+        }
+    }
 };
 
 #endif
